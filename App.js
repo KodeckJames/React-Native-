9a7ccc418f22b39d5b2f906009c847f9cf51c7ac
Kodeck James <kodeckjames095@gmail.com>
2024-08-08 12:02:10 +0300

    Ninth commit

--- App.js
+++ App.js
@@ -4,13 +4,25 @@ import ReusableBox from './ReusableBox'
 ReusableBox
 
 export default function App() {
-  return <ReusableBox />
+  return (
+    <View style={styles.container}>
+      <ReusableBox style={{ backgroundColor: 'brown' }}>Box 1</ReusableBox>
+      <ReusableBox style={{ backgroundColor: 'orange' }}>Box 2</ReusableBox>
+      <ReusableBox style={{ backgroundColor: 'plum' }}>Box 3</ReusableBox>
+      <ReusableBox style={{ backgroundColor: 'pink' }}>Box 4</ReusableBox>
+      <ReusableBox style={{ backgroundColor: 'purple' }}>Box 5</ReusableBox>
+      <ReusableBox style={{ backgroundColor: 'blue' }}>Box 6</ReusableBox>
+      <ReusableBox style={{ backgroundColor: 'crimson' }}>Box 7</ReusableBox>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    padding: 60,
+    marginTop: 64,
+    borderWidth: 6,
+    borderColor: 'red',
+    flexDirection: 'row',
   },
 })
--- ReusableBox.js
+++ ReusableBox.js
@@ -1,12 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function ReusableBox() {
+export default function ReusableBox({ children, style }) {
   return (
-    <View>
-      <Text>ReusableBox</Text>
+    <View style={[styles.box, style]}>
+      <Text style={styles.text}>{children}</Text>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  box: {
+    backgroundColor: 'white',
+    padding: 20,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+})
