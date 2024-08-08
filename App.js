4adac018d59e1825579c025ae378f5d5299654f3
Kodeck James <kodeckjames095@gmail.com>
2024-08-08 14:21:06 +0300

    Tenth commit

--- App.js
+++ App.js
@@ -21,8 +21,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: 64,
+    alignContent: 'stretch',
     borderWidth: 6,
     borderColor: 'red',
     flexDirection: 'row',
   },
 })
+// alignself is applied to individual items, allowing us to control the alignement of each item independently
+// The values available for it are similar to those used in alignItems. It's default value is auto, not stretch
+// flexWrap allows us to control how flex items behave when thre's limited space within the container ie wrap, noWrap, wrap-reverse
+// alignContent aligns lines of content along the cross axis
+// gap related properties allow us manage spacing btn rows and columns ie rowGap: 20 etc..., columnGap:20... gap: 10 => for both
