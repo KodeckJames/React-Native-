af313e2f28822d01e898f7d8db18ae5d92d588ed
Kodeck James <kodeckjames095@gmail.com>
2024-08-28 23:43:03 +0300

    Refreshing done

--- Networking.js
+++ Networking.js
@@ -6,21 +6,41 @@ import {
   Platform,
   StatusBar,
   FlatList,
+  ActivityIndicator,
 } from 'react-native'
 import React, { useState, useEffect } from 'react'
 
 export default function Networking() {
   const [postList, setpostList] = useState([])
+  const [isLoading, setisLoading] = useState(true)
+  const [refreshing, setrefreshing] = useState(false)
   const fetchData = async (limit = 10) => {
     const response = await fetch(
       `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
     )
     const data = await response.json()
     setpostList(data)
+    setisLoading(false)
   }
+
+  const handleRefresh = () => {
+    setrefreshing(true)
+    fetchData(20)
+    setrefreshing(false)
+  }
+
   useEffect(() => {
     fetchData()
   }, [])
+
+  if (isLoading) {
+    return (
+      <SafeAreaView style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <Text>Loading...</Text>
+      </SafeAreaView>
+    )
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -42,6 +62,8 @@ export default function Networking() {
           ListFooterComponent={
             <Text style={styles.footerText}>End of list</Text>
           }
+          refreshing={refreshing}
+          onRefresh={handleRefresh}
         />
       </View>
     </SafeAreaView>
@@ -115,5 +137,12 @@ const styles = StyleSheet.create({
     color: '#D8000C',
     fontSize: 16,
     textAlign: 'center',
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 })
